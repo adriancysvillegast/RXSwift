50a9fe013e37b0f9c21bc67ae3cd19b8678fa023
Adriancys Villegas <adriancysvillegast@gmail.com>
2023-06-03 11:47:42 -0400

    Initial Commit

---
+++ RickAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Adriancys Jesus Villegas Toro on 3/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
